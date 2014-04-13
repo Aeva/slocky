1e76e5d4d112f3ee10fdf9e7f638c70234ba67d8
Aeva Palecek <aeva.ntsc@gmail.com>
2014-04-13 15:46:39 -0500

    Issue #4 - building more of the gtk integration example.

--- slocky/examples/gtk_client.py
+++ slocky/examples/gtk_client.py
@@ -16,10 +16,104 @@
 
 
 import re
+import os
 import time
 import tempfile
+
+from gi.repository import Gtk
 
 from slocky.client import SlockyClient
 
 
 CLIENT_DIR = tempfile.mkdtemp()
+
+
+class GladeClient(SlockyClient):
+    def __init__(self):
+        glade_path = os.path.join(
+            os.path.split(__file__)[0],
+            "example_ui.glade")
+
+        self.__builder = Gtk.Builder()
+        self.__builder.add_from_file(glade_path)
+        self.__builder.connect_signals(self)
+
+        self.__ct_prompt = self.__builder.get_object("connection_prompt")
+        self.__ct_prompt.show_all()
+
+    def on_exit(self, *args):
+        """
+        Called when the client closes.
+        """
+        Gtk.main_quit()
+
+    def on_connect_button_pressed(self, *args):
+        """
+        Called when the client attempts to connect.
+        """
+
+        try:
+            host = self.__builder.get_object("host_entry").get_text()
+            port = self.__builder.get_object("port_entry").get_text()
+            name = self.__builder.get_object("name_entry").get_text()
+
+            host = host.strip()
+            port = int(port.strip())
+            name = name.strip()
+
+            assert len(host) > 0
+            assert len(name) > 0
+        except ValueError:
+            return
+        except AssertionError:
+            return
+
+        win = self.__ct_prompt
+        win.hide()
+
+        SlockyClient.__init__(self, host, port, CLIENT_DIR)
+        self.connect()
+
+    def on_validate(self):
+        chat = self.__chat = self.__builder.get_object("chat_window")
+        vali = self.__vali = self.__builder.get_object("validation_dialog")
+        chat.show_all()
+        vali.show_all()
+        
+    def on_validate_button_pressed(self, *args):
+        vali_entry = self.__builder.get_object("validation_entry")
+        passphrase = vali_entry.get_text().strip()
+        match = re.match(r"^[a-z]+ [a-z]+ [a-z]+ [a-z]+$", passphrase)
+        if match is not None:
+            self.__vali.hide()
+            import pdb; pdb.set_trace()
+            self.validate_device(passphrase)
+        else:            
+            print "no match"
+
+    def on_checksum_fail(self):
+        print "validation failed"
+        # fixme: show an error and make the client try again
+        #label = self.__builder.get_object("prompt_label")
+
+    def on_connect(self):
+        print "Client connected"
+        self.on_exit()
+        pass
+
+    def pulse(self):
+        """
+        Schedule me to occur periodically.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    client = GladeClient()
+    try:
+        Gtk.main()
+    except:
+        # fixme: if connection fails, re-show the connection and
+        # add an error label or something?
+        Gtk.main_quit()
+        raise
